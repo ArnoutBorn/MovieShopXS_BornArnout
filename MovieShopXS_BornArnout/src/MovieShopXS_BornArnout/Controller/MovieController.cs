bba6df072341ea6f85ae7619d9a464625f14cf4d
arnout born <arnout.born@gmail.com>
2017-05-08 09:45:28 +0200

    01

---
+++ MovieShopXS_BornArnout/src/MovieShopXS_BornArnout/Controller/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieShopXS_BornArnout
+{
+    public class MovieController : Controller
+    {
+        
+        // GET: /<controller>/
+        [Route("Movie")]
+        public IActionResult Movie( )
+        {
+            Console.WriteLine( "succes?" );
+            return View("Movie");
+        }
+        [Route("Year/{year}")]
+        public IActionResult MoviesByYear( int year )
+        {
+            return View( );
+        }
+    }
+}
